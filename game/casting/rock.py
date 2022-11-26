61e1da08ebedffaf114e11c75386b5beeb916d3d
joeverlyn <joeverlyncapute@gmail.com>
2022-11-26 14:27:57 -0700

    change rock

--- game/casting/rock.py
+++ game/casting/rock.py
@@ -1,33 +1,11 @@
-import random
-from game.casting.actor import Actor
+from game.casting.object import Object
 
-# TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
 
-# a child class 
-class Artifact(Actor):
+class Rock(Object):
     """
-    An item of cultural or historical interest. 
-    
-    The responsibility of an Artifact is to provide a message about itself.
-    Attributes:
-        _message (string): A short description about the artifact.
+    This is a basic rock
     """
     def __init__(self):
         super().__init__()
-        x = random.randint(1, 60)
-        y = random.randint(1, 40)
-        position = Point(x, y)
-        position = position.scale(15)
-
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        color = Color(r, g, b)
-    
-        self._font_size = 20
-        self._color = color
-        self._position = position
-        
-        dy = random.randint(int(VERT_SPEED / 3), VERT_SPEED)
-        dx = random.randint(- HOR_SPEED, HOR_SPEED)
-        self._velocity = Point(dx, dy)
+        self._point_value = -1
+        self._text = "O"
